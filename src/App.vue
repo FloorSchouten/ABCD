5744cb6fff8f63351aaf972d1a46cd456040c89b
FloorSchouten <70517922+FloorSchouten@users.noreply.github.com>
2023-05-16 09:09:21 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <Navigation />
+  <img
+    alt="breakfast pancakes"
+    src="./assets/pancakes.jpg"
+    style="width: 400px"
+  />
+</template>
+
+<script>
+import Navigation from "./components/Navigation.vue";
+
+export default {
+  name: "App",
+  components: {
+    Navigation,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
